75949692a8471423abf16bf5d71b909b98f6cd9a
MarcosBernal <marcosbernal3@hotmail.com>
2017-05-16 12:39:13 +0200

    New look to the interface

--- server.R
+++ server.R
@@ -1,10 +1,12 @@
 function(input, output) {
-    output$plot1 <- renderPlot({
-      plot(x = rnorm(input$n), y = rnorm(input$n))
-    })
-    output$value <- renderText({ input$obs })
-    output$vis_text <- renderText({ "Text from Case1" })
-    output$vis_status <- renderText({ "Text from Case1" })
-    output$vis_text_label <- renderText({ "Label Text" })
-    output$vis_status_label <- renderText({ "Label status" })
+ #  
+ #  output$vis_balance_city_label <- renderText({ "City" })
+ #  output$vis_balance_city <- renderText({ "Text from Case1" })
+   output$vis_balance_citizen_label <- renderText({ "Citizen " })
+   output$vis_balance_citizen <- renderText({ "0$" })
+ #  
+ #   output$vis_text <- renderText({ "Text from Case1" })
+ #    output$vis_status <- renderText({ "Text from Case1" })
+ #    output$vis_text_label <- renderText({ "Label Text" })
+ #    output$vis_status_label <- renderText({ "Label status" })
 }
--- ui.R
+++ ui.R
@@ -1,37 +1,52 @@
 navbarPage(
   "RHIME",
   tabPanel("Nice",
-           
-           
-           
            fluidPage(
              verticalLayout(
-               titlePanel("Vertical layout example"),
-               plotOutput("plot1"),
-               wellPanel(
-                 sliderInput("n", "Number of points", 10, 200,
-                             value = 50, step = 10)
-               ), numericInput("obs", "Observations:", 10, min = 1, max = 100),
-               wellPanel(  fluidRow(
-                          column(3, 
-                                 textOutput("vis_text_label")
-                                 ), 
-                          column(3,
-                               
-                               textOutput("vis_text")
-                                          )
-               ),
+               titlePanel("Balance"),
                fluidRow(
                  column(3, 
-                        textOutput("vis_status_label")
+                        textOutput("vis_balance_city_label")
                  ), 
+                 column(6,
+                        textOutput("vis_balance_city")
+                 )
+                 ),
+               fluidRow( 
                  column(3,
-                        
-                        textOutput("vis_status")
+                        textOutput("vis_balance_citizen_label")
+                 ),
+                 column(6,
+                        textOutput("vis_balance_citizen")
                  )
                )
-               )
              )
+             # ,
+             # verticalLayout(
+             #   titlePanel("Open Cases"),
+             #   wellPanel(  fluidRow(
+             #              column(3, 
+             #                     textOutput("vis_text_label")
+             #                     ), 
+             #              column(3,
+             #                   
+             #                   textOutput("vis_text")
+             #                              )
+             #   ),
+             #   fluidRow(
+             #     column(3, 
+             #            textOutput("vis_status_label")
+             #     ), 
+             #     column(3,
+             #            
+             #            textOutput("vis_status")
+             #     )
+             #   )
+             #   )
+             # ),
+             # verticalLayout(
+             #   titlePanel("Closed Cases")
+             # )
            )
            
            
